fcc42e006cfb9ed7cbb1ebf73f955bcaf1435f16
Sandor Sisak <sisaklsanyo@gmail.com>
2023-09-04 15:07:15 +0000

    noise dimensions now concaare tenated properly, added KL estimation method

--- data.py
+++ data.py
@@ -3,9 +3,9 @@ import gin
 
 @gin.configurable
 class Data:
-    def __init__(self, dataset_name, dim, alpha=1., noise_dim=0, seed=0):
+    def __init__(self, dataset_name, num_samples, alpha=1., noise_dim=0, seed=0):
         self.dataset_name = dataset_name
-        self.dim = dim
+        self.num_samples = num_samples
         self.alpha = alpha
         self.noise_dim = noise_dim
         self.seed = seed
@@ -19,13 +19,13 @@ class Data:
         elif self.dataset_name == "sphere":
             self.get_dset_sphere()
         else:
-            raise Exception("No dataset named \"{}\" is implemented".format(self.dataset_name))
+            raise NotImplementedError("No dataset named \"{}\" is implemented".format(self.dataset_name))
 
         self.add_noise()
 
     def get_dset_linear(self):
-        self.var_z = self.rng.uniform(-self.alpha/2, self.alpha/2, size=self.dim)
-        self.var_x = self.rng.uniform(0, 1, size=self.dim)
+        self.var_z = self.rng.uniform(-self.alpha/2, self.alpha/2, size=[self.num_samples, 1])
+        self.var_x = self.rng.uniform(0, 1, size=[self.num_samples, 1])
         self.var_y = self.var_x + self.var_z
 
     def get_dset_helix(self):
@@ -38,6 +38,6 @@ class Data:
         #TODO: add noise to var_x, var_y (and an option to select noise distribution?)
 
         self.var_x = np.concatenate(
-            [self.var_x, self.rng.normal(0, 1, size=self.noise_dim)]) #noise dimensions
+            [self.var_x, self.rng.normal(0, 1, size=[self.num_samples, self.noise_dim])], -1) #noise dimensions
         self.var_y = np.concatenate(
-            [self.var_y, self.rng.normal(0, 1, size=self.noise_dim)])
+            [self.var_y, self.rng.normal(0, 1, size=[self.num_samples, self.noise_dim])], -1)
--- method.py
+++ method.py
@@ -1,21 +1,43 @@
 import gin
 import wandb
 
+import numpy as np
+from scipy.spatial import distance
+from scipy.special import loggamma
+
 @gin.configurable
 class Method:
     def __init__(self, method_name, seed):
         self.method_name = method_name
         self.seed = seed
 
-        if self.method_name == "test":
-            self.eval = self.test
-
+        if self.method_name == "KL": 
+            self.eval = self.KL_estimator #Kozachenko-Leonenko estimator
         else: 
-            raise Exception("No method named \"{}\" is implemented".format(self.method_name))
+            raise NotImplementedError("No method named \"{}\" is implemented".format(self.method_name))
 
-    def test(self, var_X, var_Y):
-        if (var_X[0] >= 0 and var_Y[0] >= 0) or (var_X[0] <= 0 and var_Y[0] <= 0):
-            MI = 1
-        else:
-            MI = 0
+    def KL_estimator(self, var_x, var_y, metric="euclidean", eps=1e-10):
+        var_x = self.normalize_input(var_x)
+        var_y = self.normalize_input(var_y)
+        var_joint = np.concatenate([var_x, var_y], axis=1)
+        MI = (
+            self.kNN_entropy(var_x, 1, metric=metric, eps=eps) + 
+            self.kNN_entropy(var_y, 1, metric=metric, eps=eps) - 
+            self.kNN_entropy(var_joint, 1, metric=metric, eps=eps))
         wandb.log({"MI": MI})
+
+    def kNN_entropy(self, var_, k, metric="euclidean", eps=1e-10):
+        dim = var_.shape[-1]
+        radius = self.kNN_radius(var_, k, metric=metric, eps=eps)
+        log_mean_volume = np.mean(np.log(radius)) + (dim/2)*np.log(np.pi) - loggamma(dim/2 + 1)
+        return log_mean_volume + 0.57721
+
+    def kNN_radius(self, var_, k, metric="euclidian", eps=1e-10):
+        dist = distance.cdist(var_, var_, metric=metric)
+        dist = np.sort(dist, -1)
+        return dist[:, k] + eps
+
+    def normalize_input(self, var_):
+        var_ = var_ - np.mean(var_, axis=0)
+        var_ = var_ - np.linalg.norm(var_, axis=0)
+        return var_
