156770824ea1f980dd236932751f251eb65778b6
Sandor Sisak <sisaklsanyo@gmail.com>
2023-09-12 14:35:35 +0000

    gin config file now added as a flag

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from absl import app, flags
 import gin
 import wandb
 
@@ -15,17 +16,27 @@ class ExperimentManager:
 
     def estimate(self):
         self.method.eval(
-            exp_manager.data.var_x,
-            exp_manager.data.var_y)
+            self.data.var_x,
+            self.data.var_y)
 
-gin.parse_config_file("test.gin")
+def main(argv):
+    #parse gin file
+    gin.parse_config_files_and_bindings(FLAGS.gin_file, FLAGS.gin_param, skip_unknown=True)
 
-wandb.init(project="mi", entity="sisaklsanyo")
-wandb.config.update(gin_config_to_dict(gin.config_str()))
+    #set up wandb
+    wandb.init(project="mi", entity="sisaklsanyo")
+    wandb.config.update(gin_config_to_dict(gin.config_str()))
 
-exp_manager = ExperimentManager()
-exp_manager.estimate()
+    #run experiment
+    exp_manager = ExperimentManager()
+    exp_manager.estimate()
 
-wandb.finish()
+    wandb.finish()
 
-#print(exp_manager.__dict__)
+if __name__ == '__main__':
+    #set up gin
+    flags.DEFINE_multi_string('gin_file', None, "List of paths to the config files.")
+    flags.DEFINE_multi_string('gin_param', None, "Newline separated list of Gin param bindings.")
+    FLAGS = flags.FLAGS
+
+    app.run(main)
