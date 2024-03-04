5bd4eb3beeeb42302484df60fe6e81cff73ee7a0
Sandor Sisak <sisaklsanyo@gmail.com>
2024-03-04 20:41:45 +0000

    added gaussian dataset, true MI is now logged for both datasets, implemented MI estimators from github.com/Linear95/CLUB/tree/master

--- data.py
+++ data.py
@@ -1,20 +1,31 @@
+import gin
+import wandb
+
 import torch
 from torch import distributions
-import gin
+from numpy import log
 
 @gin.configurable
 class Data:
-    def __init__(self, dataset_name, num_samples, alpha=1., noise_dim=0, seed=0):
+    def __init__(
+        self, dataset_name, num_samples, dim=1, seed=0, 
+        noise_dim=0, alpha=1., precov_mtx=[[1., 0.], [0.5, 1.]]):
         self.dataset_name = dataset_name
         self.num_samples = num_samples
-        self.alpha = alpha
-        self.noise_dim = noise_dim
+        self.dim = dim
         self.seed = seed
+        self.noise_dim = noise_dim
+        self.alpha = alpha
+        self.precov_mtx = torch.tensor(precov_mtx) #must be lower triangular, diagonal entries all positive
+
+        assert self.precov_mtx.shape == torch.Size([2*self.dim, 2*self.dim])
 
         torch.manual_seed(self.seed)
 
         if self.dataset_name == "linear":
             self.get_dset_linear()
+        elif self.dataset_name == "gaussian":
+            self.get_dset_gaussian()
         elif self.dataset_name == "helix":
             self.get_dset_helix()
         elif self.dataset_name == "sphere":
@@ -24,12 +35,26 @@ class Data:
 
         self.add_noise()
 
+
     def get_dset_linear(self):
         self.distr_z = distributions.Uniform(-self.alpha/2, self.alpha/2)
-        self.var_z = self.distr_z.sample(sample_shape=[1, self.num_samples])
+        self.var_z = self.distr_z.sample(sample_shape=[self.dim, self.num_samples])
         self.distr_x = distributions.Uniform(0, 1)
-        self.var_x = self.distr_x.sample(sample_shape=[1, self.num_samples])
+        self.var_x = self.distr_x.sample(sample_shape=[self.dim, self.num_samples])
         self.var_y = self.var_x + self.var_z
+
+        wandb.log({"True MI": self.alpha/2 - log(self.alpha)})
+
+
+    def get_dset_gaussian(self):
+        cov_mtx = self.precov_mtx @ self.precov_mtx.T
+        self.distr_xy = distributions.MultivariateNormal(torch.zeros(2*self.dim), cov_mtx)
+        self.var_xy = self.distr_xy.sample(sample_shape=[self.num_samples]).T
+        self.var_x, self.var_y = self.var_xy[:self.dim, :], self.var_xy[self.dim:, :]
+
+        mtx_x, mtx_y = cov_mtx[:self.dim, :self.dim], cov_mtx[self.dim:, self.dim:]
+        wandb.log({"True MI": 0.5 * (torch.log(torch.det(mtx_x) * torch.det(mtx_y)) - torch.log(cov_mtx))})
+
 
     def get_dset_helix(self):
         raise NotImplementedError
--- method.py
+++ method.py
@@ -5,10 +5,14 @@ import torch
 #from scipy.spatial import distance
 from scipy.special import loggamma
 from sympy import harmonic
+from mi_estimators import *
 
 @gin.configurable
 class Method:
-    def __init__(self, method_name, seed, k=1, eps=1e-10, MI=True):
+    def __init__(
+        self, method_name, seed, k=1, eps=1e-10, MI=True, 
+        hidden_size=15, learning_rate=0.005, num_iters=100):
+
         self.method_name = method_name
         self.seed = seed
         if type(k) == list: self.k_list = k
@@ -18,18 +22,46 @@ class Method:
         elif type(eps) == float: self.eps_list = [eps]
         else: raise TypeError
         self.MI = MI
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+        self.num_iters = num_iters
         self.step = 0
 
         if self.method_name == "kNN": 
             self.eval = self.kNN_eval
+        elif self.method_name in ["NWJ", "MINE", "InfoNCE","L1OutUB","CLUB","CLUBSample"]:
+            self.eval = self.neural_eval
         else: 
             raise NotImplementedError("No method named \"{}\" is implemented".format(self.method_name))
+
 
     def kNN_eval(self, var_x, var_y):
         for eps in self.eps_list:
             for k in self.k_list:
                 self.kNN_estimator(var_x, var_y, k=k, eps=eps)
-                self.step += 1
+
+
+    def neural_eval(self, var_x, var_y):
+        dim, N = var_x.shape
+        model = eval(self.method_name)(dim, dim, self.hidden_size)
+        optimizer = torch.optim.Adam(model.parameters(), self.learning_rate)
+        mi_est_values = []
+
+        for global_iter in range(self.num_iters):
+            #batch_x, batch_y = correlated_linear(alpha=0.01, dim=sample_dim, batch_size=batch_size)
+            model.eval()
+            mi_est_values.append(model(var_x.T, var_y.T).item())
+            wandb.log({"MI": mi_est_values[-1]}, step=self.step)
+            self.step += 1
+            
+            model.train() 
+            model_loss = model.learning_loss(var_x.T, var_y.T)
+            optimizer.zero_grad()
+            model_loss.backward()
+            optimizer.step()
+            #del batch_x, batch_y
+            #torch.cuda.empty_cache()
+
 
     def kNN_estimator(self, var_x, var_y, k=1, p_norm=2, eps=1e-10):
         #var_x = self.normalize_input(var_x)
@@ -38,9 +70,12 @@ class Method:
         H_x = self.kNN_entropy(var_x, k, p_norm=p_norm, eps=eps) 
         H_y = self.kNN_entropy(var_y, k, p_norm=p_norm, eps=eps) 
         H_xy = self.kNN_entropy(var_joint, k, p_norm=p_norm, eps=eps)
+
         if self.MI: wandb.log({"MI": H_x + H_y - H_xy}, step=self.step)
         else: wandb.log({"H_x": H_x, "H_y": H_y, "H_xy": H_xy}, step=self.step)
-        wandb.log({"epsilon": eps, "k": k}, step=self.step)
+        #wandb.log({"epsilon": eps, "k": k}, step=self.step)
+        self.step += 1
+
 
     def kNN_entropy(self, var_, k=1, p_norm=2, eps=1e-10):
         dim, N = var_.shape
@@ -54,10 +89,12 @@ class Method:
             + 0.5772) #Euler-Mascheroni constant
         return log_mean_volume
 
+
     def kNN_radius(self, var_, k=1, p_norm=2, eps=1e-10):
         dist = torch.cdist(var_.T, var_.T, p=p_norm)
         dist = dist.sort().values
         return dist[:, k] + eps
+
 
     def normalize_input(self, var_): #Z-score normalization
         var_ -= var_.mean(axis=1, keepdim=True)
